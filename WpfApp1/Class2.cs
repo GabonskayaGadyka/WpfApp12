6185f6ffe73b68c38a48e43347942e996298e3c2
user <user@ILYAPC>
2024-03-01 16:11:20 +0400

    Добавьте файлы проекта.

---
+++ WpfApp1/Class2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WpfApp1
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Item> Items { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=texnika;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
+
---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void registerbutton_Click(object sender, RoutedEventArgs e)
+        {
+            Window3 register = new Window3();
+            register.Show();
+            this.Close();
+        }
+
+        private void loginbutton_Click(object sender, RoutedEventArgs e)
+        {
+            var context = new AppDbContext();
+            User user = context.Users.Where(x => x.Login == logintext.Text && x.Password == passwordtext.Text).FirstOrDefault();
+            if (user == null)
+            {
+                MessageBox.Show("Неверные данные!");
+                return;
+            }
+            Window2 menu = new Window2(user);
+            menu.Show();
+            this.Close();
+        }
+    }
+}
